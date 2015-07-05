a20f1f412badbcb23b8902fafb5ce5231a3f3b6c
Shengyou Fan <shengyoufan@gmail.com>
2015-07-05 23:00:14 +0800

    更新：處理資料格式

--- public/index.php
+++ public/index.php
@@ -16,6 +16,42 @@ $tables = $crawler->filter('table.table-B01-table')->each(function (Crawler $nod
 
 $rows = $tables[4];
 
+$contents = [];
+
+foreach($rows as $index => $row) {
+	if ($index < 15) {
+		$domCrawler = new Crawler;
+		$domCrawler->add($row);
+		$content = $domCrawler->filter('a')->each(function (Crawler $node, $index) {
+
+			$rawContent = utf8_decode($node->html());
+			$rawContent = strip_tags($rawContent);
+			$rawContent = trim($rawContent);
+			$rawContent = preg_replace("/\r|\n/", "", $rawContent);
+
+			return $rawContent;
+		});
+
+		$contents[$index]['title'] = str_replace('...', '', $content[0]);
+		$contents[$index]['from'] = $content[1];
+	}
+}
+
+foreach($rows as $index => $row) {
+	if ($index < 15) {
+		$rawContent = explode('</a>', $row);
+
+		$step1 = str_replace(')</span>', '', $rawContent[2]);
+		$step1 = str_replace('</td>', '', $step1);
+		$step1 = preg_replace("/\r|\n/", "", $step1);
+
+		$step2 = explode('點閱率', $step1);
+		
+		$contents[$index]['date'] = $step2[0];
+		$contents[$index]['rank'] = (int) $step2[1];
+	}
+}
+
 echo '<pre>';
-var_dump($rows);
+var_dump($contents);
 echo '</pre>';
